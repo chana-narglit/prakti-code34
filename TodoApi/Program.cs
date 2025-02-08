7d4fa971b6f274c18fce2af1921991a4a2bf9e28
chani <c0548457653@gmail.com>
2025-02-08 22:17:35 +0200

    Your commit message

---
+++ TodoApi/Program.cs
@@ -0,0 +1,61 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi;
+using ToDoDbContext = TodoApi.ToDoDbContext;
+using Task = TodoApi.Task;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ToDoDbContext>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder => builder.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowAllOrigins");
+
+app.MapGet("/task", async (ToDoDbContext db) =>{
+var a= await db.Tasks.ToListAsync();
+return Results.Ok(a);
+});
+
+app.MapPost("/task", async ( ToDoDbContext db, Task newTask) => {
+    await db.Tasks.AddAsync(newTask);
+    await db.SaveChangesAsync();
+    return Results.Created($"/task/{newTask.Id}", newTask);
+});
+
+app.MapPut("/task/{id}", async (ToDoDbContext db ,int id,bool inputTask) =>
+{
+    var task = await db.Tasks.FindAsync(id);
+
+    if (task is null) return Results.NotFound();
+    task.IsComplete = !task.IsComplete;
+    await db.SaveChangesAsync();
+    return Results.Ok(task);
+});
+app.MapDelete("/task/{id}", async (ToDoDbContext db, int id) =>
+{
+    var task = await db.Tasks.FindAsync(id);
+
+    if (task is null) return Results.NotFound();
+
+    db.Tasks.Remove(task);
+    await db.SaveChangesAsync();
+
+    return Results.Ok();
+});
+
+app.Run();
